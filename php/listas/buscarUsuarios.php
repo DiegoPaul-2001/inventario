3810cc616eb71cae998277453ba324d075676bf7
DiegoPaul-2001 <diegoquimbit2001@gmail.com>
2021-07-08 19:18:03 -0500

    s

--- php/listas/buscarUsuarios.php
+++ php/listas/buscarUsuarios.php
@@ -22,7 +22,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
     <!-- (Optional) Latest compiled and minified JavaScript translation files -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script>
-    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href="../../css/estilos.css">
 
     <title>Document</title>
 </head>    
